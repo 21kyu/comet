5bdcaf95ce0c57828a59f275f85ec460229bf1a3
21kyu <kyu21@outlook.com>
2023-01-12 22:48:34 +0900

    fix: add IfInfoMessage struct

--- crates/netlink/src/consts.rs
+++ crates/netlink/src/consts.rs
@@ -1,6 +1,17 @@
+pub const NLMSG_ALIGNTO: usize = 4;
 pub const NLMSG_ERROR: u16 = libc::NLMSG_ERROR as u16;
 pub const NLMSG_DONE: u16 = libc::NLMSG_DONE as u16;
 pub const NLMSG_HDRLEN: usize = 16;
 
-pub const RECV_BUF_SIZE: usize = 4096;
+pub const RECV_BUF_SIZE: usize = 65536;
 pub const PID_KERNEL: u32 = 0;
+
+pub const IFF_UP: u32 = 0x1;
+pub const IFF_BROADCAST: u32 = 0x2;
+pub const IFF_DEBUG: u32 = 0x4;
+pub const IFF_LOOPBACK: u32 = 0x8;
+pub const IFF_POINTOPOINT: u32 = 0x10;
+pub const IFF_NOTRAILERS: u32 = 0x20;
+pub const IFF_RUNNING: u32 = 0x40;
+
+pub const IF_INFO_MSG_SIZE: usize = 0x10;
--- crates/netlink/src/socket.rs
+++ crates/netlink/src/socket.rs
@@ -53,7 +53,7 @@ impl NetlinkSocket {
         Ok(())
     }
 
-    fn recv(&self) -> Result<(NetlinkMessage, libc::sockaddr_nl)> {
+    fn recv(&self) -> Result<(Vec<NetlinkMessage>, libc::sockaddr_nl)> {
         let mut from: libc::sockaddr_nl = unsafe { std::mem::zeroed() };
         let mut buf: [u8; consts::RECV_BUF_SIZE] = [0; consts::RECV_BUF_SIZE];
         let ret = unsafe {
@@ -69,8 +69,8 @@ impl NetlinkSocket {
         if ret < 0 {
             return Err(Error::last_os_error());
         }
-        let netlink_msg = NetlinkMessage::from(&buf[..ret as usize])?;
-        Ok((netlink_msg, from))
+        let netlink_msgs = NetlinkMessage::from(&buf[..ret as usize])?;
+        Ok((netlink_msgs, from))
     }
 
     fn pid(&self) -> Result<u32> {
@@ -101,18 +101,28 @@ struct NetlinkMessage {
 }
 
 impl NetlinkMessage {
-    fn from(buf: &[u8]) -> Result<Self> {
-        if buf.len() < consts::NLMSG_HDRLEN {
-            return Err(Error::new(
-                std::io::ErrorKind::InvalidData,
-                "received message is too short",
-            ));
+    fn from(mut buf: &[u8]) -> Result<Vec<Self>> {
+        let mut msgs = Vec::new();
+
+        while buf.len() >= consts::NLMSG_HDRLEN {
+            let header = unsafe { *(buf.as_ptr() as *const libc::nlmsghdr) };
+            let len = Self::nlm_align_of(header.nlmsg_len as usize);
+
+            let data = buf[consts::NLMSG_HDRLEN..].to_vec();
+
+            let m = Self {
+                header,
+                data: data[..header.nlmsg_len as usize - consts::NLMSG_HDRLEN].to_vec(),
+            };
+            msgs.push(m);
+            buf = &buf[len..];
         }
 
-        let header = unsafe { *(buf.as_ptr() as *const libc::nlmsghdr) };
-        let data = buf[consts::NLMSG_HDRLEN..header.nlmsg_len as usize].to_vec();
+        Ok(msgs)
+    }
 
-        Ok(Self { header, data })
+    fn nlm_align_of(len: usize) -> usize {
+        (len + consts::NLMSG_ALIGNTO - 1) & !(consts::NLMSG_ALIGNTO - 1)
     }
 }
 
@@ -131,9 +141,66 @@ impl fmt::Debug for NetlinkMessage {
     }
 }
 
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct IfInfoMessage {
+    ifi_family: u8,
+    _ifi_pad: u8,
+    ifi_type: u16,
+    ifi_index: i32,
+    ifi_flags: u32,
+    ifi_change: u32,
+}
+
+impl IfInfoMessage {
+    fn deserialize(buf: &[u8]) -> Result<Self> {
+        Ok(unsafe { *(buf[..consts::IF_INFO_MSG_SIZE].as_ptr() as *const Self) })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[rustfmt::skip]
+    static NETLINK_MSG: [u8; 96] = [
+        0x00, // interface family
+        0x00, // reserved
+        0x04, 0x03, // link layer type 772 = loopback
+        0x01, 0x00, 0x00, 0x00, // interface index = 1
+        0x49, 0x00, 0x00, 0x00, // device flags: UP, LOOPBACK, RUNNING, LOWERUP
+        0x00, 0x00, 0x00, 0x00, // reserved 2 (aka device change flag)
+
+        // nlas
+        0x07, 0x00, 0x03, 0x00, 0x6c, 0x6f, 0x00, // device name L=7,T=3,V=lo
+        0x00, // padding
+        0x08, 0x00, 0x0d, 0x00, 0xe8, 0x03, 0x00, 0x00, // TxQueue length L=8,T=13,V=1000
+        0x05, 0x00, 0x10, 0x00, 0x00, // OperState L=5,T=16,V=0 (unknown)
+        0x00, 0x00, 0x00, // padding
+        0x05, 0x00, 0x11, 0x00, 0x00, // Link mode L=5,T=17,V=0
+        0x00, 0x00, 0x00, // padding
+        0x08, 0x00, 0x04, 0x00, 0x00, 0x00, 0x01, 0x00, // MTU L=8,T=4,V=65536
+        0x08, 0x00, 0x1b, 0x00, 0x00, 0x00, 0x00, 0x00, // Group L=8,T=27,V=9
+        0x08, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x00, 0x00, // Promiscuity L=8,T=30,V=0
+        0x08, 0x00, 0x1f, 0x00, 0x01, 0x00, 0x00, 0x00, // Number of Tx Queues L=8,T=31,V=1
+        0x08, 0x00, 0x28, 0x00, 0xff, 0xff, 0x00, 0x00, // Maximum GSO segment count L=8,T=40,V=65536
+        0x08, 0x00, 0x29, 0x00, 0x00, 0x00, 0x01, 0x00, // Maximum GSO size L=8,T=41,V=65536
+    ];
+
+    #[test]
+    fn test_if_info_message() {
+        let msg = IfInfoMessage::deserialize(&NETLINK_MSG).unwrap();
+
+        assert_eq!(msg.ifi_family, 0);
+        assert_eq!(msg._ifi_pad, 0);
+        assert_eq!(msg.ifi_type, 772);
+        assert_eq!(msg.ifi_index, 1);
+        assert_eq!(
+            msg.ifi_flags,
+            consts::IFF_UP | consts::IFF_LOOPBACK | consts::IFF_RUNNING
+        );
+        assert_eq!(msg.ifi_change, 0);
+    }
 
     #[test]
     fn test_netlink_socket() {
@@ -150,8 +217,8 @@ mod tests {
         let pid = s.pid().unwrap();
         let mut res: Vec<Vec<u8>> = Vec::new();
 
-        loop {
-            let (netlink_msg, from) = s.recv().unwrap();
+        'done: loop {
+            let (netlink_msgs, from) = s.recv().unwrap();
             println!(
                 "received from {} {} {}",
                 from.nl_family, from.nl_pid, from.nl_groups
@@ -162,26 +229,36 @@ mod tests {
                 continue;
             }
 
-            if netlink_msg.header.nlmsg_pid != pid {
-                println!("received message with wrong pid");
-                continue;
-            }
+            for m in netlink_msgs {
+                println!("received message: {:?}", m);
 
-            match netlink_msg.header.nlmsg_type {
-                consts::NLMSG_ERROR => {
-                    println!("the kernel responded with an error");
-                    return;
+                if m.header.nlmsg_pid != pid {
+                    println!("received message with wrong pid");
+                    continue;
                 }
-                consts::NLMSG_DONE => {
-                    println!("end of dump");
-                    break;
-                }
-                _ => {
-                    res.push(netlink_msg.data);
+
+                match m.header.nlmsg_type {
+                    consts::NLMSG_ERROR => {
+                        println!("the kernel responded with an error");
+                        return;
+                    }
+                    consts::NLMSG_DONE => {
+                        println!("end of dump");
+                        break 'done;
+                    }
+                    _ => {
+                        res.push(m.data);
+                    }
                 }
             }
         }
 
         println!("res: {:?}", res);
+        println!("res.len(): {}", res.len());
+
+        res.iter().for_each(|r| {
+            let ifi = IfInfoMessage::deserialize(r).unwrap();
+            println!("ifi: {:?}", ifi);
+        });
     }
 }
