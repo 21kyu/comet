6b84dab14c23a34ac0aabc3ca47b85d4176171bf
Wongyu Lee <kyu21@outlook.com>
2023-02-12 14:01:39 +0900

    test(netlink): init benchmark (#8)
    * test(netlink): init benchmark
    
    * test(netlink):resolve clippy failure

--- crates/netlink/src/message.rs
+++ crates/netlink/src/message.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::HashMap,
-    fmt::{self, Formatter},
-};
+use std::collections::HashMap;
 
 use anyhow::Result;
 use serde::Serialize;
@@ -11,6 +8,23 @@ use crate::{consts, request::NetlinkRequestData, utils::align_of};
 pub struct NetlinkMessage {
     pub header: NetlinkMessageHeader,
     pub data: Vec<u8>,
+}
+
+impl NetlinkMessage {
+    pub fn from(mut buf: &[u8]) -> std::io::Result<Vec<Self>> {
+        let mut msgs = Vec::new();
+
+        while buf.len() >= consts::NLMSG_HDRLEN {
+            let header = unsafe { *(buf.as_ptr() as *const NetlinkMessageHeader) };
+            let len = align_of(header.nlmsg_len as usize, consts::NLMSG_ALIGNTO);
+            let data = buf[consts::NLMSG_HDRLEN..header.nlmsg_len as usize].to_vec();
+
+            msgs.push(Self { header, data });
+            buf = &buf[len..];
+        }
+
+        Ok(msgs)
+    }
 }
 
 #[repr(C)]
@@ -35,49 +49,10 @@ impl NetlinkMessageHeader {
     }
 }
 
-impl NetlinkMessage {
-    pub fn from(mut buf: &[u8]) -> std::io::Result<Vec<Self>> {
-        let mut msgs = Vec::new();
-
-        while buf.len() >= consts::NLMSG_HDRLEN {
-            let header = unsafe { *(buf.as_ptr() as *const NetlinkMessageHeader) };
-            let len = align_of(header.nlmsg_len as usize, consts::NLMSG_ALIGNTO);
-            let data = buf[consts::NLMSG_HDRLEN..header.nlmsg_len as usize].to_vec();
-
-            msgs.push(Self { header, data });
-            buf = &buf[len..];
-        }
-
-        Ok(msgs)
-    }
-}
-
-impl fmt::Debug for NetlinkMessage {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "NetlinkMessage {{ header: {} {} {} {} {}, data: {:?} }}",
-            self.header.nlmsg_len,
-            self.header.nlmsg_type,
-            self.header.nlmsg_flags,
-            self.header.nlmsg_seq,
-            self.header.nlmsg_pid,
-            self.data,
-        )
-    }
-}
-
 pub struct NetlinkRouteAttr {
     pub rt_attr: RtAttr,
     pub value: Vec<u8>,
     pub children: Option<Vec<Box<dyn NetlinkRequestData>>>,
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Debug)]
-pub struct RtAttr {
-    pub rta_len: u16,
-    pub rta_type: u16,
 }
 
 impl NetlinkRequestData for NetlinkRouteAttr {
@@ -89,7 +64,7 @@ impl NetlinkRequestData for NetlinkRouteAttr {
         self.rt_attr.rta_len == 0
     }
 
-    fn serialize(&self) -> anyhow::Result<Vec<u8>> {
+    fn serialize(&self) -> Result<Vec<u8>> {
         let mut buf = Vec::new();
         buf.extend_from_slice(&self.rt_attr.rta_len.to_ne_bytes());
         buf.extend_from_slice(&self.rt_attr.rta_type.to_ne_bytes());
@@ -183,24 +158,21 @@ impl NetlinkRouteAttr {
 }
 
 #[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct RtAttr {
+    pub rta_len: u16,
+    pub rta_type: u16,
+}
+
+#[repr(C)]
 #[derive(Clone, Copy, Default, Debug, Serialize)]
 pub struct IfInfoMessage {
     pub ifi_family: u8,
-    _ifi_pad: u8,
+    pub _ifi_pad: u8,
     pub ifi_type: u16,
     pub ifi_index: i32,
     pub ifi_flags: u32,
     pub ifi_change: u32,
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Default, Serialize)]
-pub struct IfAddrMessage {
-    pub ifa_family: u8,
-    pub ifa_prefix_len: u8,
-    pub ifa_flags: u8,
-    pub ifa_scope: u8,
-    pub ifa_index: u32,
 }
 
 impl NetlinkRequestData for IfInfoMessage {
@@ -210,20 +182,6 @@ impl NetlinkRequestData for IfInfoMessage {
 
     fn is_empty(&self) -> bool {
         self.ifi_family == 0
-    }
-
-    fn serialize(&self) -> Result<Vec<u8>> {
-        bincode::serialize(self).map_err(|e| e.into())
-    }
-}
-
-impl NetlinkRequestData for IfAddrMessage {
-    fn len(&self) -> usize {
-        consts::IF_ADDR_MSG_SIZE
-    }
-
-    fn is_empty(&self) -> bool {
-        self.ifa_family == 0
     }
 
     fn serialize(&self) -> Result<Vec<u8>> {
@@ -241,6 +199,30 @@ impl IfInfoMessage {
 
     pub fn deserialize(buf: &[u8]) -> Result<Self> {
         Ok(unsafe { *(buf[..consts::IF_INFO_MSG_SIZE].as_ptr() as *const Self) })
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Serialize)]
+pub struct IfAddrMessage {
+    pub ifa_family: u8,
+    pub ifa_prefix_len: u8,
+    pub ifa_flags: u8,
+    pub ifa_scope: u8,
+    pub ifa_index: u32,
+}
+
+impl NetlinkRequestData for IfAddrMessage {
+    fn len(&self) -> usize {
+        consts::IF_ADDR_MSG_SIZE
+    }
+
+    fn is_empty(&self) -> bool {
+        self.ifa_family == 0
+    }
+
+    fn serialize(&self) -> Result<Vec<u8>> {
+        bincode::serialize(self).map_err(|e| e.into())
     }
 }
 
